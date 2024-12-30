312cb83c0b6aed589bd8b319e4231de3d719b2e9
vaishnavi1702-v <vaishnavitalwar50@gmail.com>
2024-12-30 19:53:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navigation = document.querySelector(".primary-navigation");
-    
-    if (navigation) {
-        const navigationHeight = navigation.offsetHeight;
-        document.documentElement.style.setProperty(
-            "--scroll-padding", 
-            navigationHeight + "px"
-        );
-    }
-});
 
